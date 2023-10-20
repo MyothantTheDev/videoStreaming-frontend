50af3a0ed69ee5562f928b601391c15ec9a814c2
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-21 00:31:03 +0630

    Add Student, Batch and Video Sences

---
+++ src/sences/batch/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../conponents/layout/Header";
+
+const Batch = () => {
+    return (
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+            </Box>
+        </Box>
+    )
+}
+
+export default Batch;
