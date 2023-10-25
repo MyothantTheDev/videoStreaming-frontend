3e44175e676bc1565506ea74fb3aa6ad15cf6f0f
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-26 00:16:01 +0630

    Complete Show All Student DashBoard

--- src/sences/dashboard/index.jsx
+++ src/sences/dashboard/index.jsx
@@ -1,29 +1,44 @@
 import { Box } from "@mui/material";
 import Header from "../../conponents/layout/Header";
 import { DataGrid } from "@mui/x-data-grid";
-import { useState, useLayoutEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getAllStudents } from "../../actions/studentsAction";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllStudents } from "../../actions/studentsAction"
+import Loader from "../../conponents/layout/loader";
 
 const Dashboard = () => {
 
-    const [row, setRow] = useState([]);
     const dispatch = useDispatch();
-    useLayoutEffect(() => {
+    const { loading, students, studentsCount } = useSelector(state => state.student);
+
+    const DataRow = () => {
+        const cleanedData = [];
+        for (let i = 0; i < studentsCount; i++) {
+            const id = i + 1;
+            const student = students[i];
+            const studentData = {
+                id,
+                ...student,
+                batch: student.batchId.name
+            };
+            delete studentData.batchId
+            cleanedData.push(studentData);
+        }
+        return cleanedData;
+    }
+
+    useEffect(() => {
         dispatch(getAllStudents());
     },[dispatch])
 
-    const rows = [
-        { id: 1, col1: 'Hello', col2: 'World' },
-        { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-        { id: 3, col1: 'MUI', col2: 'is Amazing' },
-      ];
       
-      const columns = [
-        { field: 'id' , headerName: 'id', width: 100 }, 
-        { field: 'col1', headerName: 'Name', width: 150 },
-        { field: 'col2', headerName: 'Column 2', width: 150 },
-      ];
+    const columns = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    { field: 'username' , headerName: 'Student Name', width: 200 }, 
+    { field: 'email', headerName: 'Email', width: 200 },
+    { field: 'role', headerName: 'Role', width: 150 },
+    { field: 'batch', headerName: 'Batch', width: 150 },
+    ];
 
 
     return (
@@ -31,11 +46,13 @@ const Dashboard = () => {
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
             </Box>
-            <Box>
-                <DataGrid columns={columns} rows={rows}>
-
-                </DataGrid>
-            </Box>
+            {
+                !loading && (students.length) ? (
+                    <Box>
+                        <DataGrid columns={columns} rows={DataRow()}/>
+                    </Box>
+                ) : <Loader />
+            }
         </Box>
     )
 }
