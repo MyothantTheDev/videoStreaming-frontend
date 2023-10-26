1aaeca950a62be3cdcbac5e79a74fa1c0b50a141
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-27 01:40:07 +0630

    add register user reducer

--- src/reducers/studentReducers.js
+++ src/reducers/studentReducers.js
@@ -1,8 +1,11 @@
-import { ALL_STUDENTS_REQUEST, ALL_STUDENTS_SUCCESS, ALL_STUDENTS_FAIL, CLEAR_ERRORS } from "../constants/studentConstant";
+import { ALL_STUDENTS_REQUEST, ALL_STUDENTS_SUCCESS, ALL_STUDENTS_FAIL, 
+REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL,
+CLEAR_ERRORS } from "../constants/studentConstant";
 
 export const studentsReducer = (state = { students : []}, action) =>  {
     switch (action.type) {
         case ALL_STUDENTS_REQUEST:
+        case REGISTER_USER_REQUEST:
             return {
                 loading: true,
                 students: []
@@ -15,7 +18,15 @@ export const studentsReducer = (state = { students : []}, action) =>  {
                 studentsCount: action.payload.count
             };
 
+        case REGISTER_USER_SUCCESS:
+            return {
+                loading: false,
+                students: action.payload.student,
+                message: action.payload.message
+            }
+
         case ALL_STUDENTS_FAIL:
+        case REGISTER_USER_FAIL:
             return {
                 loading: false,
                 error: action.payload
