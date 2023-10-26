6d6c754c1252439cfbbf18001510a284f09ee39a
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-27 01:40:30 +0630

    remove register user reducer

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,11 +1,9 @@
 import { LOGIN_SUCCESS, LOGIN_REQUEST, LOGIN_FAIL, CLEAR_ERRORS, 
-REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL,
 LOAD_USER_SUCCESS, LOAD_USER_REQUEST, LOAD_USER_FAIL, LOGOUT_SUCCESS, LOGOUT_FAIL } from "../constants/userConstants";
 
 export const authReducer = (state = { user: {} }, action ) => {
 	switch (action.type) {
 		case LOGIN_REQUEST:
-		case REGISTER_USER_REQUEST:
 		case LOAD_USER_REQUEST:
 			return {
 				loading: true,
@@ -13,7 +11,6 @@ export const authReducer = (state = { user: {} }, action ) => {
 			};
 	
 		case LOGIN_SUCCESS:
-		case REGISTER_USER_SUCCESS:
 		case LOAD_USER_SUCCESS:
 			return {
 				...state,
@@ -39,7 +36,6 @@ export const authReducer = (state = { user: {} }, action ) => {
 			}
 		
 		case LOGIN_FAIL:
-		case REGISTER_USER_FAIL:
 			return {
 				...state,
 				loading: false,
