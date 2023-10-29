5d2be1732a3066ca27c95fc0c41c4b56d494fa2d
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-29 09:55:08 +0630

    add delete student account action

--- src/actions/studentsAction.js
+++ src/actions/studentsAction.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 import { ALL_STUDENTS_REQUEST, ALL_STUDENTS_SUCCESS, ALL_STUDENTS_FAIL, 
-REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL,CLEAR_ERRORS } from "../constants/studentConstant";
+REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL,
+DELETE_STUDENT_REQUEST, DELETE_STUDENT_SUCCESS, DELETE_STUDENT_FAIL , CLEAR_ERRORS } from "../constants/studentConstant";
 
 //Get ALL STUDENTS
 
-export const getAllStudents = () => async (disptach) => {
-    try {
+export const getAllStudents = () => (disptach) => {
 
         disptach({ type: ALL_STUDENTS_REQUEST });
-        const { data } = await axios.get('/api/v1/admin/students');
-        disptach({
-            type: ALL_STUDENTS_SUCCESS,
-            payload: data
-        })
-        
-    } catch (error) {
-        disptach({
-            type: ALL_STUDENTS_FAIL,
-            payload: error
-        })
-    }
+        axios.get('/api/v1/admin/students').then((response) => {
+			disptach({
+				type: ALL_STUDENTS_SUCCESS,
+				payload: response.data
+			})
+		})
+    	.catch(error => {
+			disptach({
+				type: ALL_STUDENTS_FAIL,
+				payload: error
+			})
+		})
 }
 
 //Register
@@ -29,7 +29,7 @@ export const register = (userData) => async (dispatch) => {
 			dispatch({ type: REGISTER_USER_REQUEST })
 			const config = {
 				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded'
+					'Content-Type': 'application/json'
 				}
 			}
 			const { data } = await axios.post('/api/v1/admin/student/new', userData, config)
@@ -40,6 +40,26 @@ export const register = (userData) => async (dispatch) => {
 	} catch (error) {
 		dispatch({
 			type: REGISTER_USER_FAIL,
+			payload: error
+		})
+	}
+}
+
+// DELETE STUDENT
+
+export const deleteStudent = (id) => async (dispatch) => {
+	try {
+		dispatch({ type: DELETE_STUDENT_REQUEST })
+
+		const { data } = await axios.delete(`/api/v1/admin/students?_id=${id}`);
+		dispatch({
+			type: DELETE_STUDENT_SUCCESS,
+			payload: data
+		})
+
+	} catch (error) {
+		dispatch({
+			type: DELETE_STUDENT_FAIL,
 			payload: error
 		})
 	}
