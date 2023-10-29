ee85ea775d49b6bbe5c95a771d01d06be0965ed1
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-29 09:57:22 +0630

    add student delete reducer

--- src/reducers/studentReducers.js
+++ src/reducers/studentReducers.js
@@ -1,8 +1,10 @@
 import { ALL_STUDENTS_REQUEST, ALL_STUDENTS_SUCCESS, ALL_STUDENTS_FAIL, 
-REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL,
-CLEAR_ERRORS } from "../constants/studentConstant";
+    REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, REGISTER_USER_FAIL,
+    DELETE_STUDENT_REQUEST, DELETE_STUDENT_SUCCESS, DELETE_STUDENT_FAIL , CLEAR_ERRORS } from "../constants/studentConstant";
+
 
 export const studentsReducer = (state = { students : []}, action) =>  {
+
     switch (action.type) {
         case ALL_STUDENTS_REQUEST:
         case REGISTER_USER_REQUEST:
@@ -10,6 +12,12 @@ export const studentsReducer = (state = { students : []}, action) =>  {
                 loading: true,
                 students: []
             };
+
+        case DELETE_STUDENT_REQUEST:
+            return {
+                loading: true,
+                students: state.students
+            }
             
         case ALL_STUDENTS_SUCCESS:
             return {
@@ -17,6 +25,13 @@ export const studentsReducer = (state = { students : []}, action) =>  {
                 students: action.payload.student,
                 studentsCount: action.payload.count
             };
+
+        case DELETE_STUDENT_SUCCESS:
+            return {
+                students: state.students.filter(i => i._id !== action.payload.student._id),
+                loading: false,
+                isDeleted: action.payload
+            }
 
         case REGISTER_USER_SUCCESS:
             return {
@@ -27,6 +42,7 @@ export const studentsReducer = (state = { students : []}, action) =>  {
 
         case ALL_STUDENTS_FAIL:
         case REGISTER_USER_FAIL:
+        case DELETE_STUDENT_FAIL:
             return {
                 loading: false,
                 error: action.payload
