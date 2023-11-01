bbbe7e327e5ce85744d6a5af63873e3443b060ab
Myo Thant <jamesrobinson636478@gmail.com>
2023-11-01 14:13:54 +0630

    CRUD for batch api

--- src/reducers/batchReducer.js
+++ src/reducers/batchReducer.js
@@ -1,8 +1,10 @@
-import { ALL_BATCH_REQUEST, ALL_BATCH_SUCCESS, ALL_BATCH_FAIL, CLEAR_ERRORS } from "../constants/batchConstants";
+import { ALL_BATCH_REQUEST, ALL_BATCH_SUCCESS, ALL_BATCH_FAIL, 
+    REGISTER_BATCH_REQUEST, REGISTER_BATCH_SUCCESS, REGISTER_BATCH_FAIL, CLEAR_ERRORS } from "../constants/batchConstants";
 
 export const batchReducer = (state = { batch : []}, action) =>  {
     switch (action.type) {
         case ALL_BATCH_REQUEST:
+        case REGISTER_BATCH_REQUEST:
             return {
                 loading: true,
                 batch: []
@@ -15,7 +17,15 @@ export const batchReducer = (state = { batch : []}, action) =>  {
                 batchCount: action.payload.count
             };
 
+        case REGISTER_BATCH_SUCCESS:
+            return {
+                loading: false,
+                batch: action.payload.batch,
+                message: action.payload.message
+            }
+
         case ALL_BATCH_FAIL:
+        case REGISTER_BATCH_FAIL:
             return {
                 loading: false,
                 error: action.payload
