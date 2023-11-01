5f3d996f3a41b16a57ca729d325308f04a085a42
Myo Thant <jamesrobinson636478@gmail.com>
2023-11-01 14:09:12 +0630

    add batch action features

--- src/actions/batchAction.js
+++ src/actions/batchAction.js
@@ -1,4 +1,5 @@
-import { ALL_BATCH_REQUEST, ALL_BATCH_SUCCESS, ALL_BATCH_FAIL, CLEAR_ERRORS } from "../constants/batchConstants";
+import { ALL_BATCH_REQUEST, ALL_BATCH_SUCCESS, ALL_BATCH_FAIL, 
+    REGISTER_BATCH_REQUEST, REGISTER_BATCH_SUCCESS, REGISTER_BATCH_FAIL, CLEAR_ERRORS } from "../constants/batchConstants";
 import axios from "axios";
 
 //GET ALL BATCH
@@ -16,6 +17,30 @@ export const getAllBatch = () => async (disptach) => {
     } catch (error) {
         disptach({
             type: ALL_BATCH_FAIL,
+            payload: error
+        })
+    }
+}
+
+// Create New Batch
+
+export const newBatch = (batchName) => async (dispatch) => {
+    try {
+
+        dispatch({ type: REGISTER_BATCH_REQUEST })
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const { data } = await axios.post('/api/v1//admin/batch/new', batchName, config);
+        dispatch({
+            type: REGISTER_BATCH_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: REGISTER_BATCH_FAIL,
             payload: error
         })
     }
