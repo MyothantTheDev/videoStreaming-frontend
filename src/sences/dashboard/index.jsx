380c34c54a258fede2b14c22042da6f9951d60e7
Myo Thant <jamesrobinson636478@gmail.com>
2023-11-04 22:08:45 +0630

    Add video data grid

--- src/sences/dashboard/index.jsx
+++ src/sences/dashboard/index.jsx
@@ -1,14 +1,16 @@
 import Header from "../../conponents/layout/Header";
 import { Box } from "@mui/material";
 import UserDataGrid from "./userDataGrid";
+import VideoDataGrid from "./videoDataGrid";
 
 const Dashboard = () => {
     return (
-        <Box m='20px'>
+        <Box m='20px' >
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
             </Box>
             <UserDataGrid />
+            <VideoDataGrid />
         </Box>
     )
 }
