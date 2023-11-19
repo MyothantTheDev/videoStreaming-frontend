97b4471a72294062044ffa300a283728f714700a
Myo Thant <jamesrobinson636478@gmail.com>
2023-11-19 20:43:04 +0630

    Create User Home

--- src/sences/home/userHome.jsx
+++ src/sences/home/userHome.jsx
@@ -1,26 +1,61 @@
 import { ColorModeContext, useMode } from '../../theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import Topbar from '../global/topBar';
-import SideBar from '../global/SideBar';
+import UserSideBar from '../global/userSideBar';
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { getBatchVideo, clearErrors } from '../../actions/videoAction';
+import VideoPlayer from '../../conponents/layout/videoPlayer';
 
 const UserHome = () => {
-    const [theme, colorMode] = useMode();
+  const [theme, colorMode] = useMode();
 
-    return (
+  const [dataFetched, setDataFetched] = useState(false);
+
+  const { video, error } = useSelector(state => state.video);
+  const { user } = useSelector(state => state.auth);
+  const { videoId } = useSelector(state => state.singleVideo);
+
+  const dispatch = useDispatch();
+
+  // DataFetch
+  useEffect(() => {
+    if (!dataFetched) {
+      dispatch(getBatchVideo(user.batchId))
+      setDataFetched(true)
+    }
+  },[dispatch, dataFetched])
+
+  //
+  useEffect(() => {
+    if (error) {
+      dispatch(clearErrors())
+    }
+  },[dispatch, error])
+
+  return (
+    <>
+    {
+      video && (
         <ColorModeContext.Provider value={colorMode} >
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <div className="app">
-                    <SideBar/>
-                    <main className="Content">
-                        <Topbar/>
-
-                    </main>
-                </div>
-            </ThemeProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="app">
+              <UserSideBar objects={video} />
+              <main className="Content">
+                <Topbar/>
+                <Routes>
+                  <Route path='video/:id' element={<VideoPlayer videoId={videoId}/>} />
+                </Routes>
+              </main>
+            </div>
+          </ThemeProvider>
         </ColorModeContext.Provider>
-    )
+      )
+    }
+    </>
+  )
 }
 
 export default UserHome;
